bfa8513e9154514593841e2e62ddf35bb2e6dbd8
Vincent Bu (Centific Technologies Inc) false <v-bibu@microsoft.com>
2023-11-29 17:51:29 +0800

    add gcperfsim rerunning function

--- actions/compare.py
+++ actions/compare.py
@@ -1,14 +1,15 @@
 import os
+import yaml
 
 from utils.terminal import run_command_sync
 
 
 def generate_configuration(
-        configuration_folder: os.PathLike,
-        output_path: os.PathLike, 
-        performance_root: os.PathLike, 
-        runtime_baseline_root: os.PathLike, 
-        runtime_target_root: os.PathLike):
+    configuration_folder: os.PathLike,
+    output_path: os.PathLike, 
+    performance_root: os.PathLike, 
+    runtime_baseline_root: os.PathLike, 
+    runtime_target_root: os.PathLike):
     print('generate configuration')
     if not os.path.exists(configuration_folder): os.makedirs(configuration_folder)
 
@@ -62,6 +63,7 @@ source_path:
 
 
 def run_comparison(performance_root: os.PathLike, configuration_folder: os.PathLike):
+    print('run gc performance comparison')
     infrastructure_root = os.path.join(
         performance_root, 'artifacts', 'bin', 'GC.Infrastructure','Release', 'net7.0'
     )
@@ -74,3 +76,60 @@ def run_comparison(performance_root: os.PathLike, configuration_folder: os.PathL
 
     command = f'{infrastructure_bin} run --configuration {run_yaml_path}'.split(' ')
     run_command_sync(command, cwd=infrastructure_root)
+
+
+def generate_gcperfsim_configuration(
+    configuration_path: os.PathLike,
+    output_root: os.PathLike,
+    loops: int,
+    scenario: str):
+    print('generate gcperfsim configuration')
+    if not os.path.exists(output_root): os.makedirs(output_root)
+
+    new_configuration_root = os.path.join(output_root, 'configuration')
+    if not os.path.exists(new_configuration_root): os.makedirs(new_configuration_root)
+
+    new_result_root = os.path.join(output_root, 'result')
+    if not os.path.exists(new_result_root): os.makedirs(new_result_root)
+
+    with open(configuration_path, 'r') as f:
+        config_yaml = yaml.load(f, yaml.Loader)
+        for run_name in config_yaml['runs'].keys():
+            if run_name != scenario: config_yaml['runs'].pop(run_name)
+
+        configuration_base_name, ext_name = os.path.splitext(os.path.basename(configuration_path))
+        for loop in range(loops):
+            new_configuration_path = os.path.join(
+                new_configuration_root,
+                f'{configuration_base_name}_{scenario}_{loop+1}{ext_name}'
+            )
+            new_result_path = os.path.join(new_result_root, f'{configuration_base_name}_{scenario}_{loop+1}')
+            with open(new_configuration_path, 'w+') as f:
+                config_yaml['output']['path'] = new_result_path
+                yaml.dump(config_yaml, f, default_flow_style=False)
+
+
+def compare_gcperfsim(
+    performance_root: os.PathLike,
+    output_root: os.PathLike,
+    loops: int):
+    print(f'run gcperfsim {loops} times')
+    
+    infrastructure_root = os.path.join(
+        performance_root, 'artifacts', 'bin', 'GC.Infrastructure','Release', 'net7.0'
+    )
+    infrastructure_bin = os.path.join(
+        infrastructure_root, 'GC.Infrastructure.exe'
+    )
+
+    new_configuration_root = os.path.join(output_root, 'configuration')
+    for idx, gcperfsim_configuration_name in enumerate(os.listdir(new_configuration_root)):
+        print(f'loop {idx+1}')
+        new_configuration_path = os.path.join(new_configuration_root, gcperfsim_configuration_name)
+    
+        command = f'{infrastructure_bin} gcperfsim --configuration {new_configuration_path}'.split(' ')
+        run_command_sync(command, cwd=infrastructure_root)
+
+
+def compare_microbenchmarks(times: int=10):
+    pass
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-import sys
+import argparse
 
 import config
 from utils.init import init_test
@@ -7,66 +7,141 @@ from utils.init import init_test
 
 if __name__ == '__main__':
     init_test()
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest='action')
 
-    action = sys.argv[1]
+    parser_download = subparsers.add_parser('download')
+    parser_download.add_argument(
+        '-r',
+        '--repo', 
+        dest='download_repo',
+        choices=['performance', 'baseline', 'target', 'crank']
+    )
 
-    if action == 'download':
-        
-        from actions import download
-        performance_root = os.path.join(config.test_bed, 'performance')
-        runtime_baseline_root = os.path.join(config.test_bed, config.runtime_baseline_name)
-        runtime_target_root = os.path.join(config.test_bed, config.runtime_target_name)
-        if len(sys.argv) <= 2:
-            download.download_performance(performance_root)
-            download.download_runtime(runtime_baseline_root, config.runtime_baseline_tag_number)
-            download.download_runtime(runtime_target_root, config.runtime_target_tag_number)
-            download.install_tool()
-        else:
-            repo = sys.argv[2]
-            if repo == 'performance': download.download_performance(performance_root)
-            elif repo == 'baseline': download.download_runtime(runtime_baseline_root, config.runtime_baseline_tag_number)
-            elif repo == 'target': download.download_runtime(runtime_target_root, config.runtime_target_tag_number)
-            elif repo == 'crank': download.install_tool()
-            else: raise Exception(f'unknown repo: {repo}')
+    parser_build = subparsers.add_parser('build')
+    parser_build.add_argument(
+        '-r',
+        '--repo', 
+        dest='build_repo',
+        choices=['performance', 'baseline', 'target']
+    )
 
-    elif action == 'build':
-        import config
-        from actions import build
+    parser_test = subparsers.add_parser('test')
+    parser_build.add_argument(
+        '-n',
+        '--name', 
+        dest='test_name',
+        choices=['gcperfsim', 'microbenchmark', 'aspnet']
+    )
+    parser_build.add_argument(
+        '-s',
+        '--scenario', 
+        dest='scenario',
+        type=str
+    )
 
-        performance_root = os.path.join(config.test_bed, 'performance')
-        runtime_baseline_root = os.path.join(config.test_bed, config.runtime_baseline_name)
-        runtime_target_root = os.path.join(config.test_bed, config.runtime_target_name)
+    parser_build.add_argument(
+        '-l',
+        '--loops', 
+        dest='loops',
+        type=int
+    )
 
-        if len(sys.argv) <= 2:
-            build.build_runtime(runtime_baseline_root, config.vcvars64_activation_path)
-            build.build_runtime(runtime_target_root, config.vcvars64_activation_path)
-            build.build_Infrastructure(performance_root)
-            build.build_GCPerfSim(performance_root)
-            build.build_Microbenchmarks(performance_root)
-        else:
-            repo = sys.argv[2]
-            if repo == 'performance': 
-                build.build_Infrastructure(performance_root)
-                build.build_GCPerfSim(performance_root)
-                build.build_Microbenchmarks(performance_root)
-            elif repo == 'baseline': build.build_runtime(runtime_baseline_root, config.vcvars64_activation_path)
-            elif repo == 'target': build.build_runtime(runtime_target_root, config.vcvars64_activation_path)
-            else: raise Exception(f'unknown repo: {repo}')
-    elif action == 'test':
-        from actions import compare
-        
-        configuration_folder = os.path.join(config.test_bed, 'Configurations')
-        comparison_name = f'{config.runtime_target_name}_vs_{config.runtime_baseline_name}'
-        output_folder = os.path.join(config.test_bed, 'Outputs', comparison_name)
-        performance_root = os.path.join(config.test_bed, 'performance')
-        runtime_baseline_root = os.path.join(config.test_bed, config.runtime_baseline_name)
-        runtime_target_root = os.path.join(config.test_bed, config.runtime_target_name)
+    args = parser.parse_args()
+    
+    action = args.action
 
-        compare.generate_configuration(configuration_folder, 
-                                       output_folder,performance_root, runtime_baseline_root, runtime_target_root)
-        compare.run_comparison(performance_root, configuration_folder)
-    elif action == 'clean':
-        from actions import clean
-        clean.remove_dotnet_temp()
-    else:
-        raise Exception(f'unknown action: {action}')
+    match action:
+        case 'download':
+            from actions import download
+            performance_root = os.path.join(config.test_bed, 'performance')
+            runtime_baseline_root = os.path.join(config.test_bed, config.runtime_baseline_name)
+            runtime_target_root = os.path.join(config.test_bed, config.runtime_target_name)
+            repo = args.download_repo
+            match repo:
+                case 'performance': download.download_performance(performance_root)
+                case 'baseline': download.download_runtime(runtime_baseline_root, config.runtime_baseline_tag_number)
+                case 'target': download.download_runtime(runtime_target_root, config.runtime_target_tag_number)
+                case 'crank': download.install_tool()
+                case None: 
+                    download.download_performance(performance_root)
+                    download.download_runtime(runtime_baseline_root, config.runtime_baseline_tag_number)
+                    download.download_runtime(runtime_target_root, config.runtime_target_tag_number)
+                    download.install_tool()
+
+        case 'build':
+            import config
+            from actions import build
+
+            performance_root = os.path.join(config.test_bed, 'performance')
+            runtime_baseline_root = os.path.join(config.test_bed, config.runtime_baseline_name)
+            runtime_target_root = os.path.join(config.test_bed, config.runtime_target_name)
+
+            repo = args.build_repo
+            
+            match repo:
+                case 'performance': 
+                    build.build_Infrastructure(performance_root)
+                    build.build_GCPerfSim(performance_root)
+                    build.build_Microbenchmarks(performance_root)
+                case 'baseline': build.build_runtime(runtime_baseline_root, config.vcvars64_activation_path)
+                case 'target': build.build_runtime(runtime_target_root, config.vcvars64_activation_path)
+                case None:
+                    build.build_runtime(runtime_baseline_root, config.vcvars64_activation_path)
+                    build.build_runtime(runtime_target_root, config.vcvars64_activation_path)
+                    build.build_Infrastructure(performance_root)
+                    build.build_GCPerfSim(performance_root)
+                    build.build_Microbenchmarks(performance_root)
+
+        case 'test':
+            from actions import compare
+            
+            configuration_folder = os.path.join(config.test_bed, 'Configurations')
+            comparison_name = f'{config.runtime_target_name}_vs_{config.runtime_baseline_name}'
+            output_folder = os.path.join(config.test_bed, 'Outputs', comparison_name)
+            performance_root = os.path.join(config.test_bed, 'performance')
+            runtime_baseline_root = os.path.join(config.test_bed, config.runtime_baseline_name)
+            runtime_target_root = os.path.join(config.test_bed, config.runtime_target_name)
+
+            test_name = args.test_name
+            match test_name:
+                case 'gcperfsim':
+                    test_name = args.test_name
+                    loops = args.loops
+                    scenario =args.scenario
+
+                    gcperfsim_configuration_path = os.path.join(
+                        config.test_bed, 
+                        'Outputs', 
+                        comparison_name, 
+                        'Suites',
+                        'GCPerfSim',
+                        'LowVolatilityRun.yaml')
+                    
+                    gcperfsim_output_root = os.path.join(
+                        config.test_bed, 
+                        'Outputs', 
+                        comparison_name, 
+                        'GCPerfSim',
+                        f'GCPerfSim_{scenario}')
+                    compare.generate_gcperfsim_configuration(
+                        gcperfsim_configuration_path,
+                        gcperfsim_output_root,
+                        loops,
+                        scenario
+                    )
+                    compare.compare_gcperfsim(
+                        performance_root,
+                        gcperfsim_output_root,
+                        loops
+                    )
+                case None:
+                    compare.generate_configuration(configuration_folder, 
+                                                output_folder,performance_root, runtime_baseline_root, runtime_target_root)
+                    compare.run_comparison(performance_root, configuration_folder)
+
+        case 'clean':
+            from actions import clean
+            clean.remove_dotnet_temp()
+
+        case _: Exception(f'unknown action: {action}')
